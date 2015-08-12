03c9ab9224a34a83bed71617bc5ed7af6d318017
robinlan <robin_lan2003@yahoo.com.tw>
2015-08-13 00:53:44 +0800

    shit~

--- Template Panels/typer_panels.h
+++ Template Panels/typer_panels.h
@@ -1,23 +1,23 @@
 /********************************************************************************
 root_finding_panels.h, by Robin Lan - 2015/08/12
-
+				
 Example usage:
 	Global setting:
-		HWND rootfindingHwnd;
-		TCHAR szClassNameRF[ ] = _T("RootFindingPanel");
+		HWND typerHwnd;
+		static TCHAR szTyperAppName[] = TEXT("Typer") ;
 		int nGlobCmdShow;
-
+	
 	In WinMain:
-		WinWindows wincRFObject(szClassNameRF,hThisInstance,nCmdShow);
-		WNDCLASSEX wincRF = wincRFObject.getWinClass(RootFindingWindowProcedure);
-		nGlobCmdShow = nCmdShow;
-		if( !wincRFObject.getWinRegisterClass())
+		nGlobCmdShow = iCmdShow;
+		WinWindows wincTyperObject(szTyperAppName,hInstance,iCmdShow);
+		WNDCLASSEX wincTyper = wincTyperObject.getWinClass(TyperWindowProcedure);
+		if( !wincTyperObject.getWinRegisterClass())
 			return 0;
-		rootfindingHwnd = wincRFObject.getWinHWND(694,575,_T("Root finding Panel"));
-
+		typerHwnd = wincTyperObject.getWinHWND(444,275,_T("Typer Program"));
+		
 	In trigger place:
-		ShowWindow (rootfindingHwnd, nGlobCmdShow);
-
+		ShowWindow (typerHwnd, nGlobCmdShow);
+				
 ********************************************************************************/
 
 #if defined(UNICODE) && !defined(_UNICODE)
@@ -27,71 +27,10 @@ Example usage:
 #endif
 
 #include <tchar.h>
-#include <stdlib.h>
-#include <string>
 #include "res/win_widget.h"
 
 #define IDC_TYPER_BUTTON	101
 #define BUFFER(x,y) *(pBuffer + y * cxBuffer + x)
-
-using namespace std;
-
-LRESULT CALLBACK TyperWindowProcedure (HWND, UINT, WPARAM, LPARAM) ;
-LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM) ;
-
-HWND typerHwnd;
-int nGlobCmdShow;
-
-int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                    PSTR szCmdLine, int iCmdShow)
-{
-     static TCHAR szMainAppName[] = TEXT ("Main") ;
-	 static TCHAR szTyperAppName[] = TEXT ("Typer") ;
-     HWND         hwnd ;
-     MSG          msg ;
-     WNDCLASS     wndclass ;
-
-     wndclass.style         = CS_HREDRAW | CS_VREDRAW ;
-     wndclass.lpfnWndProc   = WindowProcedure ;
-     wndclass.cbClsExtra    = 0 ;
-     wndclass.cbWndExtra    = 0 ;
-     wndclass.hInstance     = hInstance ;
-     wndclass.hIcon         = LoadIcon (NULL, IDI_APPLICATION) ;
-     wndclass.hCursor       = LoadCursor (NULL, IDC_ARROW) ;
-     wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH) ;
-     wndclass.lpszMenuName  = NULL ;
-     wndclass.lpszClassName = szMainAppName ;
-
-	 nGlobCmdShow = iCmdShow;
-
-	 WinWindows wincTyperObject(szTyperAppName,hInstance,iCmdShow);
-	 WNDCLASSEX wincTyper = wincTyperObject.getWinClass(TyperWindowProcedure);
-
-     if (!RegisterClass (&wndclass)) {
-          return 0 ;
-     }
-
-	 if( !wincTyperObject.getWinRegisterClass())
-		return 0;
-
-     hwnd = CreateWindow (szMainAppName, TEXT ("Typing Program"),
-                          WS_OVERLAPPEDWINDOW,
-                          CW_USEDEFAULT, CW_USEDEFAULT,
-                          CW_USEDEFAULT, CW_USEDEFAULT,
-                          NULL, NULL, hInstance, NULL) ;
-
-	 typerHwnd = wincTyperObject.getWinHWND(444,275,_T("Typer Program"));
-
-     ShowWindow (hwnd, iCmdShow) ;
-     UpdateWindow (hwnd) ;
-
-     while (GetMessage (&msg, NULL, 0, 0))
-     {
-          TranslateMessage (&msg) ;
-          DispatchMessage (&msg) ;
-     }
-     return msg.wParam ;
-}
 
 LRESULT CALLBACK TyperWindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -103,27 +42,24 @@ LRESULT CALLBACK TyperWindowProcedure (HWND hwnd, UINT message, WPARAM wParam, L
      int            x, y, i ;
      PAINTSTRUCT    ps ;
      TEXTMETRIC     tm ;
-
-	 char orderStr[256];
-	 TCHAR order[256];
-
-     switch (message) {
-
+     
+     switch (message)
+     {
      case WM_INPUTLANGCHANGE:
           dwCharSet = wParam ;
-
+                                        // fall through
      case WM_CREATE:
           hdc = GetDC (hwnd) ;
           SelectObject (hdc, CreateFont (0, 0, 0, 0, 0, 0, 0, 0,
                                    dwCharSet, 0, 0, 0, FIXED_PITCH, NULL)) ;
-
+          
           GetTextMetrics (hdc, &tm) ;
           cxChar = tm.tmAveCharWidth ;
           cyChar = tm.tmHeight ;
-
+          
           DeleteObject (SelectObject (hdc, GetStockObject (SYSTEM_FONT))) ;
           ReleaseDC (hwnd, hdc) ;
-
+                                        // fall through                
      case WM_SIZE:
                // obtain window size in pixels
 
@@ -133,74 +69,94 @@ LRESULT CALLBACK TyperWindowProcedure (HWND hwnd, UINT message, WPARAM wParam, L
                cyClient = HIWORD (lParam) ;
           }
                // calculate window size in characters
-
+          
           cxBuffer = max (1, cxClient / cxChar) ;
           cyBuffer = max (1, cyClient / cyChar) ;
-
+          
                // allocate memory for buffer and clear it
-
+          
           if (pBuffer != NULL)
                free (pBuffer) ;
 
           pBuffer = (TCHAR *) malloc (cxBuffer * cyBuffer * sizeof (TCHAR)) ;
-
+          
           for (y = 0 ; y < cyBuffer ; y++)
                for (x = 0 ; x < cxBuffer ; x++)
-				   BUFFER(x,y) = ' ' ;
-
+                    BUFFER(x,y) = ' ' ;
+                    
                // set caret to upper left corner
 
           xCaret = 0 ;
           yCaret = 0 ;
-
+                    
           if (hwnd == GetFocus ())
                SetCaretPos (xCaret * cxChar, yCaret * cyChar) ;
 
           InvalidateRect (hwnd, NULL, TRUE) ;
           return 0 ;
-
+                    
      case WM_SETFOCUS:
+               // create and show the caret
+          
           CreateCaret (hwnd, NULL, cxChar, cyChar) ;
           SetCaretPos (xCaret * cxChar, yCaret * cyChar) ;
           ShowCaret (hwnd) ;
           return 0 ;
-
+          
      case WM_KILLFOCUS:
+               // hide and destroy the caret
+
           HideCaret (hwnd) ;
           DestroyCaret () ;
           return 0 ;
-
+          
      case WM_KEYDOWN:
           switch (wParam)
           {
           case VK_HOME:
                xCaret = 0 ;
                break ;
-
+               
           case VK_END:
                xCaret = cxBuffer - 1 ;
                break ;
-
+               
+          case VK_PRIOR:
+               yCaret = 0 ;
+               break ;
+               
+          case VK_NEXT:
+               yCaret = cyBuffer - 1 ;
+               break ;
+               
           case VK_LEFT:
                xCaret = max (xCaret - 1, 0) ;
                break ;
-
+               
           case VK_RIGHT:
                xCaret = min (xCaret + 1, cxBuffer - 1) ;
                break ;
-
+               
+          case VK_UP:
+               yCaret = max (yCaret - 1, 0) ;
+               break ;
+               
+          case VK_DOWN:
+               yCaret = min (yCaret + 1, cyBuffer - 1) ;
+               break ;
+               
           case VK_DELETE:
                for (x = xCaret ; x < cxBuffer - 1 ; x++)
                     BUFFER (x, yCaret) = BUFFER (x + 1, yCaret) ;
-
+               
                BUFFER (cxBuffer - 1, yCaret) = ' ' ;
-
+               
                HideCaret (hwnd) ;
                hdc = GetDC (hwnd) ;
-
+          
                SelectObject (hdc, CreateFont (0, 0, 0, 0, 0, 0, 0, 0,
                                    dwCharSet, 0, 0, 0, FIXED_PITCH, NULL)) ;
-
+                    
                TextOut (hdc, xCaret * cxChar, yCaret * cyChar,
                         & BUFFER (xCaret, yCaret),
                         cxBuffer - xCaret) ;
@@ -210,10 +166,9 @@ LRESULT CALLBACK TyperWindowProcedure (HWND hwnd, UINT message, WPARAM wParam, L
                ShowCaret (hwnd) ;
                break ;
           }
-
           SetCaretPos (xCaret * cxChar, yCaret * cyChar) ;
           return 0 ;
-
+          
      case WM_CHAR:
           for (i = 0 ; i < (int) LOWORD (lParam) ; i++)
           {
@@ -226,7 +181,7 @@ LRESULT CALLBACK TyperWindowProcedure (HWND hwnd, UINT message, WPARAM wParam, L
                          SendMessage (hwnd, WM_KEYDOWN, VK_DELETE, 1) ;
                     }
                     break ;
-
+                    
                case '\t':                    // tab
                     do
                     {
@@ -234,47 +189,36 @@ LRESULT CALLBACK TyperWindowProcedure (HWND hwnd, UINT message, WPARAM wParam, L
                     }
                     while (xCaret % 8 != 0) ;
                     break ;
-
+                    
                case '\n':                    // line feed
                     if (++yCaret == cyBuffer)
                          yCaret = 0 ;
-
                     break ;
-
+                    
                case '\r':                    // carriage return
-					wctomb(orderStr, order);
-
-					MessageBox(NULL,
-						order,
-						"Information",
-						MB_ICONINFORMATION);
-
-					xCaret = 0 ;
-
+                    xCaret = 0 ;
+                    
                     if (++yCaret == cyBuffer)
                          yCaret = 0 ;
-
                     break ;
-
+                    
                case '\x1B':                  // escape
                     for (y = 0 ; y < cyBuffer ; y++)
                          for (x = 0 ; x < cxBuffer ; x++)
                               BUFFER (x, y) = ' ' ;
-
+                         
                     xCaret = 0 ;
                     yCaret = 0 ;
-
+                         
                     InvalidateRect (hwnd, NULL, FALSE) ;
                     break ;
-
+                         
                default:                      // character codes
                     BUFFER (xCaret, yCaret) = (TCHAR) wParam ;
-
-					order[xCaret] = (TCHAR) wParam;
-
+                    
                     HideCaret (hwnd) ;
                     hdc = GetDC (hwnd) ;
-
+          
                     SelectObject (hdc, CreateFont (0, 0, 0, 0, 0, 0, 0, 0,
                                    dwCharSet, 0, 0, 0, FIXED_PITCH, NULL)) ;
 
@@ -289,65 +233,30 @@ LRESULT CALLBACK TyperWindowProcedure (HWND hwnd, UINT message, WPARAM wParam, L
                     if (++xCaret == cxBuffer)
                     {
                          xCaret = 0 ;
-
+                         
                          if (++yCaret == cyBuffer)
                               yCaret = 0 ;
                     }
                     break ;
                }
           }
-
+          
           SetCaretPos (xCaret * cxChar, yCaret * cyChar) ;
           return 0 ;
-
+          
      case WM_PAINT:
           hdc = BeginPaint (hwnd, &ps) ;
-
+          
           SelectObject (hdc, CreateFont (0, 0, 0, 0, 0, 0, 0, 0,
                                    dwCharSet, 0, 0, 0, FIXED_PITCH, NULL)) ;
-
+                              
           for (y = 0 ; y < cyBuffer ; y++)
                TextOut (hdc, 0, y * cyChar, & BUFFER(0,y), cxBuffer) ;
 
           DeleteObject (SelectObject (hdc, GetStockObject (SYSTEM_FONT))) ;
           EndPaint (hwnd, &ps) ;
           return 0 ;
-
-     case WM_DESTROY:
-          PostQuitMessage (0) ;
-          return 0 ;
-     }
-     return DefWindowProc (hwnd, message, wParam, lParam) ;
-}
-
-LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
-	switch (message) {
-
-     case WM_CREATE: {
-
-		WinButton button1("GO",50,220,100,24,hwnd,(HMENU)IDC_TYPER_BUTTON);
-		HWND hbutton = button1.getButton();
-
-		break;
-	 }
-
-	case WM_COMMAND: {
-
-		switch(LOWORD(wParam)){
-
-			case IDC_TYPER_BUTTON: {
-
-				/* Make the window visible on the screen */
-				ShowWindow (typerHwnd, nGlobCmdShow);
-
-				break;
-			}
-
-		}
-
-		break;
-	}
-
+          
      case WM_DESTROY:
           PostQuitMessage (0) ;
           return 0 ;
